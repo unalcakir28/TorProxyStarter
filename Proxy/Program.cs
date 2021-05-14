4c388db438a32f135ef5c8022ccd8f88cf5e7dec
Ünal ÇAKIR <cakirunal28@gmail.com>
2021-05-14 19:43:31 +0300

    Gereksiz kullanımlar temizlendi.

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,6 +1,6 @@
-﻿using HtmlAgilityPack;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
@@ -15,23 +15,18 @@ namespace Proxy
 {
     public static class Program
     {
-        static int register = 0;
-        static int initializeTorRegister = 0;
-
-        static Client TorClient;
+        static Client client;
 
         static void Main(string[] args)
         {
             InitializeTor();
 
-            WebClient client = new WebClient();
-            client.Proxy = new WebProxy("127.0.0.1:8182");
-            String Text = client.DownloadString("https://baconipsum.com/api/?type=meat-and-filler");
+            WebClient webClient = new WebClient();
+            webClient.Proxy = new WebProxy(client.Proxy.Address);
+            Console.WriteLine(webClient.DownloadString("https://api.ipify.org/"));
+            client.Dispose();
         }
 
-        /// <summary>
-        /// App.config içerisindeki verileri alarak tor.exe'yi 127.0.0.1:8182 adresinden başlatır.
-        /// </summary>
         static void InitializeTor()
         {
             try
@@ -41,49 +36,26 @@ namespace Proxy
                 if (previous != null && previous.Length > 0)
                 {
                     foreach (Process process in previous)
-                    {
-                        try
-                        {
-                            process.Kill();
-                        }
-                        catch (Exception)
-                        {
-
-                        }
-                    }
-
+                        process.Kill();
                 }
 
                 ClientCreateParams createParameters = new ClientCreateParams();
-                createParameters.ConfigurationFile = System.Configuration.ConfigurationManager.AppSettings["torConfigurationFile"];
-                createParameters.ControlPassword = System.Configuration.ConfigurationManager.AppSettings["torControlPassword"];
-                createParameters.ControlPort = Convert.ToInt32(System.Configuration.ConfigurationManager.AppSettings["torControlPort"]);
-                createParameters.DefaultConfigurationFile = System.Configuration.ConfigurationManager.AppSettings["torDefaultConfigurationFile"];
-
-
-                createParameters.Path = System.Configuration.ConfigurationManager.AppSettings["torPath"];
-
+                createParameters.ConfigurationFile = ConfigurationManager.AppSettings["torConfigurationFile"];
+                createParameters.ControlPassword = ConfigurationManager.AppSettings["torControlPassword"];
+                createParameters.ControlPort = Convert.ToInt32(ConfigurationManager.AppSettings["torControlPort"]);
+                createParameters.DefaultConfigurationFile = ConfigurationManager.AppSettings["torDefaultConfigurationFile"];
+                createParameters.Path = ConfigurationManager.AppSettings["torPath"];
 
                 createParameters.SetConfig(ConfigurationNames.AvoidDiskWrites, true);
                 createParameters.SetConfig(ConfigurationNames.GeoIPFile, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip"));
                 createParameters.SetConfig(ConfigurationNames.GeoIPv6File, Path.Combine(Environment.CurrentDirectory, @"Tor\Data\Tor\geoip6"));
-                TorClient = Client.Create(createParameters);
-                TorClient.Status.GetAllRouters();
-                initializeTorRegister++;
+
+                client = Client.Create(createParameters);
             }
             catch (Exception ex)
             {
                 throw;
             }
-        }
-
-        /// <summary>
-        /// Tor ağını kapatıp tekrar açarak bağlı bulunduğu nodu değiştirir. Aktif tor nodu artık kulllanılamıyorsa bu fonksiyonun çalıştırılması gerekir.
-        /// </summary>
-        static void RefreshTor()
-        {
-            TorClient.Stop(true);
-            InitializeTor();
         }
     }
 }
